62a2dcc4658b9033879a608b8b53577c78a9d858
saaiswetha <55787747+saaiswetha@users.noreply.github.com>
2021-02-07 17:03:58 +0530

    Create FrequencyOfElement.cpp
    frequency of a particular element in a sorted array.

---
+++ FrequencyOfElement.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int firstOccurance(int a[],int n,int key){
+    int l=0,r=n-1,fi=-1; //l=left , r= right
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(a[mid]==key&&a[mid-1]==key)
+       { r=mid-1;}
+      else if(a[mid]==key&&a[mid-1]<key)
+      { fi=mid;break;}
+       else if(a[mid]>key)
+       r=mid-1;
+       else if(a[mid]<key)
+       l=mid+1;
+}
+return fi;
+}
+
+int lastOccurance(int a[],int n,int key){
+    int l=0,r=n-1,la=-1; //l=left , r= right
+    while(l<=r){
+        int mid=(l+r)/2;
+        if(a[mid]==key&&a[mid+1]==key)
+       { l=mid+1;}
+      else if(a[mid]==key&&a[mid+1]>key)
+      { la=mid;break;}
+       else if(a[mid]>key)
+       r=mid-1;
+       else if(a[mid]<key)
+       l=mid+1;
+}
+return la;
+}
+
+int main()
+{
+    int a[]={1,2,3,3,4,4,4,5,6},n=sizeof(a)/sizeof(int),l=0,r=n-1,key=4;
+    int fi=firstOccurance(a,n,key);
+    int la=lastOccurance(a,n,key);
+    cout<<la-fi+1;
+    
+}
